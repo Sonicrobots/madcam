e2c65b077fb3da750156cf8dae49c92cdafdde89
karsten gebbert <k@ioctl.it>
2015-02-06 01:01:47 +0100

    recalculate total width

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -65,6 +65,7 @@ void ofApp::mouseReleased(int x, int y, int button){
 void ofApp::windowResized(int w, int h){
   winWidth  = w;
   winHeight = h;
+  scaledWidth = calculate_width();
 }
 
 //--------------------------------------------------------------
