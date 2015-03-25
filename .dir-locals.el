0cb698333983b51ee3bb8aec25a6c95a4cc9cb8e
karsten gebbert <k@ioctl.it>
2015-03-25 23:10:12 +0100

    keep it for reference..

---
+++ .dir-locals.el
@@ -0,0 +1,16 @@
+((nil . ((company-c-headers-path-system "/usr/include/c++/4.9.2/")
+         (company-clang-arguments . ("-I/home/k/src/projects/multicam/src/"
+                                     "-I/usr/include/c++/4.9.2/"
+                                     "-I/usr/include/c++/4.9.2/x86_64-unknown-linux-gnu"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/math"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/communication"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/video"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/events"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/types"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/graphics"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/utils"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/app"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/gl"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/sound"
+                                     "-I/home/k/src/misc/openFrameworks/libs/openFrameworks/3d")))))
